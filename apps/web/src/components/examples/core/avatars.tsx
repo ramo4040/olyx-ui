38538e293419d6d4ebb8dea23d3278904b0bb26a
Votre Nom <votre.email@domain.com>
2025-12-27 19:40:46 +0100

    fix: avatar & button

--- apps/web/src/components/examples/core/avatars.tsx
+++ apps/web/src/components/examples/core/avatars.tsx
@@ -16,6 +16,12 @@ export const AvatarExamples = () => {
 					<AvatarFallback>LT</AvatarFallback>
 				</Avatar>
 
+				<Avatar size={50} shape="circle">
+					<AvatarImage src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?w=128&h=128&dpr=2&q=80" />
+					<AvatarFallback>LT</AvatarFallback>
+					<AvatarBadge status="online" />
+				</Avatar>
+
 				<Avatar size={50} shape="square">
 					<AvatarImage src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?w=128&h=128&dpr=2&q=80" />
 					<AvatarFallback>LT</AvatarFallback>
