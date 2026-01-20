c2799b38fa275dea1c32d78b4cce772479077ba0
Votre Nom <votre.email@domain.com>
2026-01-20 17:32:43 +0100

    feat: logo

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,73 +1,9 @@
-"use client";
+import { Logo } from "@/components/misc";
 
-import { useEffect, useState } from "react";
-import { ComboboxExamples, TableExample } from "@/components/examples";
-import { Switch } from "@/components/ui";
-
-export default function Home() {
-  const [theme, setTheme] = useState(false);
-
-  useEffect(() => {
-    const html = document.documentElement;
-    html.setAttribute("data-theme", theme ? "dark" : "light");
-  }, [theme]);
-
+export default function Page() {
   return (
-    <>
-      <header
-        style={{
-          marginBottom: 100,
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
-        <Switch checked={theme} onCheckedChange={setTheme} />
-      </header>
-      <div style={{ display: "grid", gap: 40 }}>
-        {/*
-         <AvatarExamples />
-         <RadioExample />
-         <SwitchExample />
-         <TextareaExample />
-         <SpinnerExamples />
-         <CheckboxExample />
-         <KbdExample />
-         <FieldExample />
-         <SeparatorExample />
-         <AccordionExample />
-         <DrawerExample />
-         <PopoverExamples />
-         <SliderExample />
-         <ProgressExamples />
-         <CollapsibleExamples />
-         <PreviewCardExamples />
-         <TooltipExamples />
-         <ToogleExample />
-         <AlertExample />
-         <NumberFieldExamples />
-         <ContextMenuExample />
-         <ScrollAreaExample />
-         <AlertDialogExample />
-         <MenuBarExamples />
-         <ToolbarExample />
-         <InputsExample />
-         <RatingReviewExample />
-         <BreadCrumbExample />
-         <NavigationMenuExample />
-         <StatusBadgeExample />
-         <DropdownMenuExample />
-         <SelectExample />
-         <ComboboxExamples />
-         <BannerExamples />
-         <RatingBarExamples />
-         <ButtonsExamples />
-         <ToastExamples />
-        <TagExamples />
-         */}
-
-        <ComboboxExamples />
-        <TableExample />
-      </div>
-    </>
+    <div>
+      <Logo />
+    </div>
   );
 }
