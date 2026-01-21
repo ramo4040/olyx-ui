436a730ad9e562f42fc1e8d84f36b1b977f55486
Votre Nom <votre.email@domain.com>
2026-01-21 17:33:31 +0100

    feat: loading card

--- apps/web/src/widgets/examples/index.ts
+++ apps/web/src/widgets/examples/index.ts
@@ -1,3 +1,4 @@
 export * from "./edit-canvas";
 export * from "./feedback-card";
+export * from "./loading-card";
 export * from "./post-composer";
---
+++ apps/web/src/widgets/examples/loading-card/index.tsx
@@ -0,0 +1,19 @@
+import "./style.css";
+import { Button, Spinner } from "@/components/ui";
+
+export const LoadingCard = () => {
+  return (
+    <div data-ui="loading-card">
+      <div className="spinner">
+        <Spinner />
+      </div>
+
+      <h3>Just a moment...</h3>
+      <p>Working our magic. This should only take a few seconds.</p>
+
+      <Button variant="primary" mode="lighter">
+        Cancel
+      </Button>
+    </div>
+  );
+};
