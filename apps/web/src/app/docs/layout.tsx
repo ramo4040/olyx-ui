37d9e7d4e1fe2b3cc19687aaf0da145c9e55fc46
Votre Nom <votre.email@domain.com>
2026-01-30 02:39:08 +0100

    feat: docs sidebar

--- apps/web/src/app/docs/layout.tsx
+++ apps/web/src/app/docs/layout.tsx
@@ -1,11 +1,14 @@
-import { MainNavbar } from "@/widgets/misc";
 import "./style.css";
+import { source } from "@/lib/source";
+import { DocsSidebar, MainNavbar } from "@/widgets/misc";
 
 export default function DocsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  console.log(source.pageTree);
+
   return (
     <div data-ui="docs-layout">
       <header data-ui="docs-header">
@@ -13,7 +16,7 @@ export default function DocsLayout({
       </header>
 
       <main>
-        sidebar
+        <DocsSidebar tree={source.pageTree} />
         {children}
       </main>
     </div>
---
+++ apps/web/src/widgets/misc/docs-sidebar/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@olyx/react";
+import "./style.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import type { source } from "@/lib/source";
+
+type Props = {
+  tree: typeof source.pageTree;
+};
+
+export const DocsSidebar = ({ tree }: Props) => {
+  const pathname = usePathname();
+
+  return (
+    <aside data-ui="docs-sidebar">
+      <NavigationMenu orientation="vertical">
+        {tree.children.map((item) => (
+          <NavigationMenuList key={item.$id}>
+            <h5>{item.name}</h5>
+
+            {item.type === "folder" &&
+              item.children.map((child) => {
+                return child.type === "page" ? (
+                  <NavigationMenuItem key={child.$id}>
+                    <NavigationMenuLink
+                      className="link"
+                      active={child.url === pathname}
+                      render={<Link href={{ pathname: child.url }} />}
+                    >
+                      {child.name}
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                ) : null;
+              })}
+          </NavigationMenuList>
+        ))}
+      </NavigationMenu>
+    </aside>
+  );
+};
