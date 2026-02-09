fa9cc6983ac4ba74cb1d50a66738ae9d92c72125
Yassir Rouane <yasserro100@gmail.com>
2026-02-09 23:55:13 +0100

    feat: mobil navbar

--- apps/web/src/widgets/misc/mobile-navbar/index.tsx
+++ apps/web/src/widgets/misc/mobile-navbar/index.tsx
@@ -1,12 +1,10 @@
 import "./style.css";
-import { ScrollArea } from "@olyx/react";
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTrigger,
-} from "@olyx/react/drawer";
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@olyx/react/sheet";
 import Link from "next/link";
 import type { source } from "@/lib/source";
 
@@ -19,52 +17,39 @@ export const MobileNavbar = ({
   items: { href: string; label: string }[];
   tree: typeof source.pageTree;
 }) => {
-  console.log(tree);
-
   return (
-    <Drawer direction="left">
-      <DrawerTrigger render={children} />
-      <DrawerContent className="mobile-navbar-drawer">
-        <DrawerHeader>
-          <DrawerClose />
-        </DrawerHeader>
+    <Sheet>
+      <SheetTrigger render={children} />
+      <SheetContent side="left" className="mobile-navbar-drawer">
+        <SheetClose />
+        <div className="links-container">
+          <h3>Menu</h3>
+          {items.map((e) => (
+            <Link key={e.label} href={{ pathname: e.href }} className="link">
+              {e.label}
+            </Link>
+          ))}
+        </div>
 
-        <ScrollArea>
-          <div className="links-container">
-            <div>
-              <h3>Menu</h3>
-              {items.map((e) => (
-                <Link
-                  key={e.label}
-                  href={{ pathname: e.href }}
-                  className="link"
-                >
-                  {e.label}
-                </Link>
-              ))}
-            </div>
+        {tree.children.map((item) => (
+          <div key={item.$id} className="links-container">
+            <h3>{item.name}</h3>
 
-            {tree.children.map((item) => (
-              <div key={item.$id}>
-                <h3>{item.name}</h3>
-
-                {item.type === "folder" &&
-                  item.children.map((child) => {
-                    return child.type === "page" ? (
-                      <Link
-                        key={child.$id}
-                        href={{ pathname: child.url }}
-                        className="link"
-                      >
-                        {child.name}
-                      </Link>
-                    ) : null;
-                  })}
-              </div>
-            ))}
+            {item.type === "folder" &&
+              item.children.map((child) => {
+                return child.type === "page" ? (
+                  <Link
+                    key={child.$id}
+                    href={{ pathname: child.url }}
+                    className="link"
+                  >
+                    {child.name}
+                  </Link>
+                ) : null;
+              })}
           </div>
-        </ScrollArea>
-      </DrawerContent>
-    </Drawer>
+        ))}
+      </SheetContent>
+    </Sheet>
   );
 };
